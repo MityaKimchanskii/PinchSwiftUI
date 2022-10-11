3a3a54d02b32305b20f08bf36cb6a3328999c4f2
Dmitrii Kim <mitya_kim@icloud.com>
2022-10-11 11:10:49 -0700

    Initial Commit

---
+++ PinchSwiftUI/PinchSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchSwiftUIApp.swift
+//  PinchSwiftUI
+//
+//  Created by Mitya Kim on 10/11/22.
+//
+
+import SwiftUI
+
+@main
+struct PinchSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
